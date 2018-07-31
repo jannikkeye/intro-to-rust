763f642d60363514443e6f197313bd88d96b72e3
Jannik Keye <jannik.keye@gmail.com>
2018-07-31 14:54:39 +0200

    feat: add crates chapter

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,13 @@
-use std::fmt;
+extern crate example_crate;
+extern crate rand;
 
-pub mod example_mod_file;
-pub mod example_mod_folder;
-
-mod example_basic {
-    use super::*;
-    #[derive(Debug)]
-    pub struct Struct {
-        string: &'static str,
-        ch: char,
-    }
-
-    impl Struct {
-        pub fn new() -> Self {
-            Struct {
-                string: "'ello!",
-                ch: 'H',
-            }
-        }
-    }
-
-    impl fmt::Display for Struct {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "{}", self.string.replace("'", &self.ch.to_string()))
-        }
-    }
-}
+use rand::prelude::*;
 
 fn main() {
-    let example_basic = example_basic::Struct::new();
-    let example_mod_file = example_mod_file::Struct::new();
-    let example_mod_folder = example_mod_folder::Struct::new();
-    println!("{} from example_basic\n", example_basic);
-    println!("{} from example_mod_file\n", example_mod_file);
-    println!("{} from example_mod_folder\n", example_mod_folder);
+    let example_crate = example_crate::Struct::new();
+    println!("{} from example_crate\n", example_crate);
+
+    let random_int: u8 = random();
+
+    println!("{}", random_int)
 }
